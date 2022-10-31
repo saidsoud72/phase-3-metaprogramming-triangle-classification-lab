fb80f146fbbf5f5a3c99eaa9ad4614f5cef75fcc
saidsoud72 <said.omar@student.moringaschool.com>
2022-10-31 13:18:06 +0300

    complete assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,29 +1,36 @@
-attr_accessor :sides
-def initialize(a, b, c)
-  self.sides = a, b, c
-end
-def kind
-  raise TriangleError if zero_or_negative_side? || !satisfies_triangle_inequality?
-  @kind ||= case unique_side_lengths
-            when 3
-              :scalene
-            when 2
-              :isosceles
-            when 1
-              :equilateral
-            end
-end
-class TriangleError < StandardError
-  "This is not a Valid triangle!"
-end
-private
-def unique_side_lengths
-  sides.uniq.count
-end
-def zero_or_negative_side?
-  sides.any? {|side| side <= 0 }
-end
-def satisfies_triangle_inequality?
-  sides.inject(:+) > 2 * sides.max
-end
+class Triangle
+  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    triangle_validation
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def positive_number?
+    [a, b, c].all?(&:positive?)
+  end
+
+  def triangle_inequality?
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def triangle_validation
+    raise TriangleError unless positive_number? && triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
 end
